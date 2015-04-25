744379fdbb15434811dbbbd8f96d0529a969ceff
Nishanth <Nishanth@Nishanth-PC>
2015-04-24 20:06:00 -0400

    Person

--- FriendshipGraphs/src/graphs/Person.java
+++ FriendshipGraphs/src/graphs/Person.java
@@ -1,5 +1,38 @@
 package graphs;
 
-public class Person {
+import java.util.ArrayList;
 
+public class Person {
+	private String name, school;
+	private ArrayList<Person> friends;
+	
+	public Person(String n, String college)
+	{
+		this.name = n;
+		this.school = college;
+		friends = new ArrayList<Person>();
+	}
+	
+	public void addFriend(Person p)
+	{
+		friends.add(p);
+	}
+	
+	public String toString()
+	{
+		return name + " " + school;
+	}
+	
+	public String getFriends()
+	{
+		String x = "";
+		
+		for(int i = 0;i < friends.size();i++)
+		{
+			x += friends.get(i) + "-->";
+		}
+		
+		return x;
+	}
 }
+
