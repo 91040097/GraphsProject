c00f45bcf0ba45d4a8304afce4399f16c62f30a0
Ramaseshan Parthasarathy <tilluboy@gmail.com>
2015-04-24 20:50:03 -0400

    class name change
    Friends.java

---
+++ FriendshipGraphs/src/graphs/Friends.java
@@ -0,0 +1,28 @@
+package graphs;
+import java.util.*;
+
+public class Friends {
+
+	/**
+	 * HashMap of the people	
+	 */
+	private HashMap<String, Person> people;
+	
+	/**
+	 * Build the graph from input file
+	 * Create the people HashMap
+	 * @param file the input file to be read in
+	 */
+	public FriendshipGraph(String file){
+		people = new HashMap<String, Person>();
+		build(file);
+	}
+	
+	/**
+	 * Builds a Graph
+	 * @param file input file
+	 */
+	private static void build(String file){
+		
+	}
+}
